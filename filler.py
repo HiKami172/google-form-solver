9d4c2ba5aef5a846312cf154f9dc5ec0eb772d1a
DESKTOP-OFMMG60\HiKami <hikami172@gmail.com>
2023-09-08 20:02:34 +0200

    gitignore update

--- filler.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-option.add_argument('-incognito')
-# option.add_argument("--headless")
-# option.add_argument("disable-gpu")
-
-browser = webdriver.Chrome(options=option)
-browser.get('browser.get("https://forms.gle/FoAoauz53Xy7A4n68")')
