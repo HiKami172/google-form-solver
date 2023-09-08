6cf1895d41e118e7b45ae2fe42e75f2113ee2f53
DESKTOP-OFMMG60\HiKami <hikami172@gmail.com>
2023-09-08 19:04:08 +0200

    selenium + requests implementation.

--- form_solver.py
+++ form_solver.py
@@ -4,6 +4,8 @@ import validators
 import re
 import yaml
 
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -15,20 +17,16 @@ def load_config(filepath):
     return config
 
 
-def fetch_html(url, driver_options):
+def setup_driver(url, driver_options):
     options = webdriver.ChromeOptions()
-    args = driver_options.get('args')
+    args = driver_options.get('args', [])
     for arg in args:
         options.add_argument(f"--{arg}")
 
     driver = webdriver.Chrome(options)
     driver.get(url)
 
-    html = driver.page_source
-    soup = BeautifulSoup(html, 'html.parser')
-
-    driver.close()
-    return soup
+    return driver
 
 
 def is_valid_url(url, patterns):
@@ -43,61 +41,67 @@ def solve(url, answers_file, config="config.yml"):
 
     url_patterns = cfg.get('url_patterns')
     post_headers = cfg.get('headers')
-    driver_options = cfg.get('driver_options')
+    driver_options = cfg.get('driver_options', {})
 
     #####################################################
     if not is_valid_url(url, url_patterns):
         raise ValueError("Invalid google form url")
 
-    soup = fetch_html(url, driver_options)
+    driver = setup_driver(url, driver_options)
 
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
     form = soup.find('form')
-    entry_tags = form.find_all(
-        name='input',
-        attrs={
-            'type': 'hidden',
-            'name': re.compile('^entry\.\d{9,10}$')
-        },
-    )
-    sentinel_tags = form.find_all(
-        name='input',
-        attrs={
-            'type': 'hidden',
-            'name': re.compile('^entry\.\d{9,10}_sentinel$')
-        },
-    )
-
-    entries = [tag['name'] for tag in entry_tags]
-    sentinels = [tag['name'] for tag in sentinel_tags]
-    for sentinel in sentinels:
-        entries.append(sentinel.removesuffix('_sentinel'))
 
     answers_df = pd.read_csv(answers_file, header=None)
     answers = answers_df.iloc[0].values
 
-    additional_payload = {
-        'dlut': 1694128079771,
-        'fvv': 1,
-        'partialResponse': [None, None, "-2430334359103828238"],
-        'pageHistory': 0,
-        'fbzx': -2430334359103828238,
-    }
+    payload = create_payload(form, answers)
 
     post_url = form['action']
-    payload = {entry: answer for entry, answer in zip(entries, answers)}
-    for sentinel in sentinels:
-        payload[sentinel] = ''
 
-    payload.update(additional_payload)
-    session = requests.Session()
-    response = session.post(post_url, headers=post_headers, data=payload)
+    response = requests.post(post_url, headers=post_headers, data=payload)
+
+    driver.close()
 
     try:
         response.raise_for_status()
     except requests.exceptions.HTTPError as e:
         return f"Post error: {e}"
 
-    session.close()
+
+def create_payload(form, answers):
+    payload = {}
+
+    entry_pattern = re.compile('^entry\.\d{9,10}$')
+    sentinel_pattern = re.compile('^entry\.\d{9,10}_sentinel$')
+
+    entry_tags = form.find_all('input', attrs={'type': 'hidden', 'name': entry_pattern})
+    sentinel_tags = form.find_all('input', attrs={'type': 'hidden', 'name': sentinel_pattern})
+
+    entries = [tag['name'] for tag in entry_tags]
+
+    sentinels = [tag['name'] for tag in sentinel_tags]
+    entries.extend([sentinel.removesuffix('_sentinel') for sentinel in sentinels])
+
+    dlut = int(time.time() * 1e3)
+    fvv = form.find('input', attrs={'type': 'hidden', 'name': 'fvv'})['value']
+    partial_response = form.find('input', attrs={'type': 'hidden', 'name': 'partialResponse'})['value']
+    page_history = form.find('input', attrs={'type': 'hidden', 'name': 'pageHistory'})['value']
+    fbzx = form.find('input', attrs={'type': 'hidden', 'name': 'fbzx'})['value']
+
+    core_values = {
+        'dlut': dlut,  # unix timestamp
+        'fvv': fvv,  # google form version
+        'partialResponse': partial_response,  # form completion flag
+        'pageHistory': page_history,  # user's path through form
+        'fbzx': fbzx  # security session id
+    }
+
+    payload.update({entry: answer for entry, answer in zip(entries, answers)})
+    payload.update({sentinel: '' for sentinel in sentinels})
+    payload.update(core_values)
+    return payload
 
 
 if __name__ == '__main__':
